4dfe38f26274fc85be65c4f6e9319e86f478622b
zDefcon <antholozadarequelme@gmail.com>
2026-04-15 05:43:01 -0500

    feat: Implement BubbleSort method for sorting arrays

--- src/OrdenamientoBurbuja/BubbleSort.java
+++ src/OrdenamientoBurbuja/BubbleSort.java
@@ -4,4 +4,19 @@ public class BubbleSort {
     public static void main(String[] args) {
          int[] nums = {5, 1, 4, 2, 8};
     }
+
+    public static int[] BubbleSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j+1]){
+                    int aux = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = aux;
+                }
+            }
+
+        }
+
+        return arr;
+    }
 }
