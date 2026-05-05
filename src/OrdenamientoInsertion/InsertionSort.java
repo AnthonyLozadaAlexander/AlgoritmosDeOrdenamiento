d6edcffb1ce06b55f5c1a271dc7bc80c12ed7674
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-05 01:37:11 -0500

    feat: Implement InsertionSort algorithm

---
+++ src/OrdenamientoInsertion/InsertionSort.java
@@ -0,0 +1,23 @@
+﻿package OrdenamientoInsertion;
+
+public class InsertionSort {
+    static void main(String[] args) {
+     int[] arr = {4,5,2,7,8};
+    }
+
+    static void insertionSort(int[] num){
+        int n = num.length;
+        // empezamos desde [1] porque asumimos que [0] ya esta ordenado
+        for (int i = 1; i < n; i++) {
+            int elemento = num[i];
+            int j = i - 1;
+
+            while (j >= 0 && num[j] > elemento) {
+                num[j + 1] = num[j];
+                j = j - 1;
+            }
+
+            num[j + 1]= elemento;
+        }
+    }
+}
